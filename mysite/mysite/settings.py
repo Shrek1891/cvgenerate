3a6d387a82f320cf3122b5071e202d8f7fc9ed13
unknown <zhur_zhur@ukr.net>
2025-08-31 15:51:11 +0300

    pre

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,6 +122,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+ALLOWED_HOSTS = ['*']
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
